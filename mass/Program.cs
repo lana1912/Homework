44ab54ff3c447741bf231c16f1ea741ba4188818
unknown <kovgankosvetlana.com>
2021-11-13 13:57:22 +0300

    mass1

--- mass/Program.cs
+++ mass/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int [] a= new int [10];
+for(int i =0; i<a.Length; i++)
+{
+    a[i]= new Random().Next(10);
+    Console.Write($"{a[i]}");
+
+}
+
+int [] b= new int [a.Length];
+Console.WriteLine("/n до ");
+int index=0;
+for(int i=0; i<a.Length; i++)
+{
+    if(a[i]%2==0)
+    {
+        b[index]=a[i];
+        Console.Write($"{b[index]}");
+        index++;
+    }
+}
+Console.WriteLine("/n/n после");
+for(int i=0; i<index; i++)
+{
+    Console.Write($"{b[i]}");
+}
