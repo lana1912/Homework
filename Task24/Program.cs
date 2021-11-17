8a4695259eb0aaceaffe77b933f4f857f0312f29
unknown <kovgankosvetlana.com>
2021-11-17 17:47:15 +0300

    task24

---
+++ Task24/Program.cs
@@ -0,0 +1,11 @@
+﻿//24.НАЙТИ КУБЫ ЧИСЕЛ ОТ 1 ДО N.
+void Method(int N)
+{
+  for(int count=1;count<=N;count++)
+  {
+    Console.WriteLine($"Квадварт числа {count}={count*count*count}");
+  }
+}
+Console.Write("Введите число: ");
+int N=Convert.ToInt32(Console.ReadLine());
+Method(N);
