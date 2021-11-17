e37f7e3c029056dc13eb6c34e855170554457377
unknown <kovgankosvetlana.com>
2021-11-17 12:37:20 +0300

    task00

---
+++ Task00/Program.cs
@@ -0,0 +1,9 @@
+﻿//00.ВЫВЕСТИ КВАДРАТ ЧИСЛА 
+ 
+ void Method(int b)
+ {
+   Console.WriteLine($"Квадрат числа b: {b*b}");
+ }
+ Console.Write("Введите число b : ");
+ int b=Convert.ToInt32(Console.ReadLine());
+ Method(b);
