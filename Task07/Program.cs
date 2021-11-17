9fd6df72d610586c11fe054b4d41f005886a6669
unknown <kovgankosvetlana.com>
2021-11-17 14:22:29 +0300

    task07

---
+++ Task07/Program.cs
@@ -0,0 +1,15 @@
+﻿//07.ПОКАЗАТЬ ЧИСЛА ОТ - N ДО +N.
+void number(int N)
+{
+ int minN= N*-1;
+ int count=minN;
+ while(count<=N)
+ {
+   Console.WriteLine(count);
+   count++;
+ } 
+}
+Console.Write("Введите число N: ");
+int N=Convert.ToInt32(Console.ReadLine());
+number(N);
+
