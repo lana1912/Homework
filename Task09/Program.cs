246737955b0cfc04105a2b9603313575e71df157
unknown <kovgankosvetlana.com>
2021-11-17 15:31:37 +0300

    task9

---
+++ Task09/Program.cs
@@ -0,0 +1,10 @@
+﻿//09.ПОКАЗАТЬ ПОСЛЕДНЮЮ ЦИФРУ ТРЕХЗНАЧНОГО ЧИСЛА.
+
+void number(int C)
+{
+    int A=C%10;
+    Console.WriteLine(A);
+}
+int C=new Random().Next(100,999);
+Console.WriteLine(C);
+number(C);
