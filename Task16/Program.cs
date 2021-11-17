4b384fc857df3d1d6f74f89db3f22200d757a833
unknown <kovgankosvetlana.com>
2021-11-17 16:28:54 +0300

    task16

---
+++ Task16/Program.cs
@@ -0,0 +1,14 @@
+﻿string method(int a)
+{
+  string [] days= {"Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскреснье"};
+  string b= Convert.ToString(days[a-1]);
+  int c=6;
+  int d=7;
+  if (a==c) {Console.WriteLine("Выходной день");}
+  if (a==d) {Console.WriteLine("Выходной день");}
+
+  return b;
+}
+Console.Write("Введите день недели: ");
+int a=Convert.ToUInt16(Console.ReadLine());
+Console.Write(method(a)); 
