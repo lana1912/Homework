d74a5febfa697c85f54ddc8760f3949907559254
unknown <kovgankosvetlana.com>
2021-11-17 18:18:30 +0300

    task27

---
+++ Task27/Program.cs
@@ -0,0 +1,17 @@
+﻿// 27. Определить количество цифр в числе
+
+int CountDigit(int n)
+{
+    int k=0;
+    for (k =0; n!=0; k++)
+    {
+        n =n/10;
+
+    }
+ return k;
+}
+Console.WriteLine("Введите число");
+int n =Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Количество цифр в числе");
+Console.WriteLine(CountDigit(n));
+
