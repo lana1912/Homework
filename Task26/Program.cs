0ec9e028631451f3a97c4e5c8eff48e1d2fb374d
unknown <kovgankosvetlana.com>
2021-11-17 18:04:05 +0300

    task26

---
+++ Task26/Program.cs
@@ -0,0 +1,11 @@
+﻿// Возведите число А в натуральную степень B используя цикл
+double degree(double a, double b)
+{
+    return Math.Pow(a, b);
+}
+Console.WriteLine("Введите число a");
+double a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число b");
+double b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Число a в степени b");
+Console.WriteLine(degree(a,b));
