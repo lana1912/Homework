489e8a6b6bcef0d1511d6707df2cc2e865188904
unknown <kovgankosvetlana.com>
2021-11-17 17:54:49 +0300

    task25

---
+++ Task25/Program.cs
@@ -0,0 +1,14 @@
+﻿//25.НАЙТИ СУММУ ЧИСЕЛ ОТ 1 ДО A.
+void Sum(int a,int sum)
+{
+  for(int count=0;count<=a;count++)
+  {
+     sum=sum+count;
+  }
+  Console.WriteLine(sum);
+}
+Console.WriteLine("введите число  а: ");
+int a= Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Сумма чисел от 1 до {a} ");
+int sum=0;
+Sum(a,sum); 
