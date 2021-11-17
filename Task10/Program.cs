52ec523838dcdf3d39789b1e819482fdbe527978
unknown <kovgankosvetlana.com>
2021-11-17 15:34:13 +0300

    task10

---
+++ Task10/Program.cs
@@ -0,0 +1,11 @@
+﻿//10.ПОКАЗАТЬ ВТОРУЮ ЦИФРУ ТРЕХЗНАЧНОГО ЧИСЛА.
+
+void number(int C)
+{
+    int A=C%100/10;
+    Console.WriteLine(A);
+}
+int C=new Random().Next(100,999);
+Console.WriteLine(C);
+number(C);
+
