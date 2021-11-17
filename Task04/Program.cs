c48d2abd8a8f6d5e6ec810bb96da584ae4064b73
unknown <kovgankosvetlana.com>
2021-11-17 13:53:34 +0300

    task04

---
+++ Task04/Program.cs
@@ -0,0 +1,17 @@
+﻿//04.НАЙТИ МАКСИМАЛЬНОЕ ИЗ ТРЕХ ЧИСЕЛ.
+
+  void number(int A, int B, int C)
+{
+    int max=0;
+    A= max;
+    if(B>A) {max=B;}
+    if(C>B) {max=C;}
+    Console.Write($"Максимальное из трех чисел: {max}");
+}
+Console.WriteLine("Введите число A : ");
+ int A=Convert.ToInt32(Console.ReadLine());
+ Console.WriteLine("Введите число B : ");
+ int B=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число C : ");
+ int C=Convert.ToInt32(Console.ReadLine());
+number(A,B,C); 
