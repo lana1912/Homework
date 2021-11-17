38b0efb36c5715885e12b186374a9a6cc6a5e521
unknown <kovgankosvetlana.com>
2021-11-17 15:14:55 +0300

    task08

---
+++ Task08/Program.cs
@@ -0,0 +1,16 @@
+﻿//08.ПОКАЗАТЬ ЧЕТНЫЕ ЧИСЛА ОТ 1 ДО N.
+void number(int N)
+{
+    int count=1;
+while(count<=N)
+{
+  if(count%2==0)
+  {
+    Console.WriteLine(count);
+  }
+  count++;
+}
+}
+Console.WriteLine("Введите число : ");
+int N=Convert.ToInt32(Console.ReadLine());
+number(N);
