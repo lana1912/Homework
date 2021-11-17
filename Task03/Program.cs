ac0e7f0c5d14061336a9dcac0d34e8bebbbd10f2
unknown <kovgankosvetlana.com>
2021-11-17 13:42:54 +0300

    task03

---
+++ Task03/Program.cs
@@ -0,0 +1,15 @@
+﻿//03.ПО ЗАДАНОМУ НОМЕРУ ДНЯ ВЫВЕСТИ ДЕНЬ НЕДЕЛИ.
+
+ string Method(int a)
+{
+    string [] days={"Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота","Воскресенье"};
+    string b = Convert.ToString(days[a-1]);
+
+    
+    return b;
+
+}
+Console.Write("Введите день недели: ");
+int a = Convert.ToInt16(Console.ReadLine());
+Console.Write(Method(a)); 
+  
