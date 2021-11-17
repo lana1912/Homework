660bcd5b5b9000d8085f24e7a6e054d0dcfb6c24
unknown <kovgankosvetlana.com>
2021-11-17 14:13:44 +0300

    task06

---
+++ Task06/Program.cs
@@ -0,0 +1,18 @@
+﻿//06.Выяснить является ли число четным.
+
+void Method(int a)
+{
+  if (a%2==0)
+  {
+    Console.WriteLine($"Число {a} четное");
+  }
+  else
+  {
+    Console.WriteLine($"Число {a} нечетное");
+  }
+
+}
+Console.WriteLine("Введите число а : ");
+int a=Convert.ToInt32(Console.ReadLine());
+Method(a);
+
