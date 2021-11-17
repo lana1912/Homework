5e543686c850419c74b41d6c3b97980d6bfe6c1d
unknown <kovgankosvetlana.com>
2021-11-17 17:28:45 +0300

    task23

---
+++ Task23/Program.cs
@@ -0,0 +1,11 @@
+﻿//23.ПОКАЗАТЬ ТАБЛИЦУ КВАДРАТОВ ЧИСЕЛ ОТ 1 ДО N.
+ void Method(int N)
+{
+  for(int count=1;count<=N;count++)
+  {
+    Console.WriteLine($"Квадварт числа {count}={count*count}");
+  }
+}
+Console.Write("Введите число: ");
+int N=Convert.ToInt32(Console.ReadLine());
+Method(N); 
