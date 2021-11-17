f78403d0dcb0ec266faf50ecbfa9b61eaa28f6bb
unknown <kovgankosvetlana.com>
2021-11-17 16:26:48 +0300

    task15

---
+++ Task15/Program.cs
@@ -0,0 +1,11 @@
+﻿//15.ДАНО ЧИСЛО.ПРОВЕРИТЬ КРАТНО ЛИ ОНО 7 И 23.
+ void number(int B)
+{
+  if(B%7==0) {Console.WriteLine("Число B кратно 7");}
+  else{Console.WriteLine("Число B не кратно 7");}
+  if(B%23==0) {Console.WriteLine("Число B кратно 23");}
+  else{Console.WriteLine("Число B не кратно 23");}
+}
+Console.Write("Введите число В: ");
+int B= Convert.ToInt16(Console.ReadLine());
+number(B);
