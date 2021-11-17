f09f746854919e5a57e771eebf6900d2347fd714
unknown <kovgankosvetlana.com>
2021-11-17 14:09:11 +0300

    task05

---
+++ Task05/Program.cs
@@ -0,0 +1,10 @@
+﻿//05.Написать программу вычисления значения функции y=f(x).
+
+int Function(int a)
+{
+    int y = a + 25 ;
+    return y;
+}
+Console.WriteLine("Введите число a");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Ваш ответ {Function(a)}");
