5ac17b5032dee43bf4d989dbbf42ca3456607e59
unknown <kovgankosvetlana.com>
2021-11-17 13:05:28 +0300

    task01

---
+++ Task01/Program.cs
@@ -0,0 +1,12 @@
+﻿//01.ПО ДВУМ ЗАДАНЫМ ЧИСЛАМ ПРОВЕРЯТЬ ЯВЛЯЕТСЯ ЛИ ПЕРВОЕ КВАДРАТОМ ВТОРОГО.
+
+void Method (int a, int b)
+ {
+     if (a==b*b) { Console.WriteLine($"Число a: {a} является квадратом числа b: {b}");}
+     else {Console.WriteLine($"Число a:{a} не является квадратом числа b: {b}");}
+ }
+ Console.WriteLine("Введите число а : ");
+ int a=Convert.ToInt32(Console.ReadLine());
+ Console.WriteLine("Введите число b : ");
+ int b=Convert.ToInt32(Console.ReadLine());
+ Method (a,b); 
