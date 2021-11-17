b0bf345e05b25646d39ea9250da6de4ae560ae5b
unknown <kovgankosvetlana.com>
2021-11-17 15:39:23 +0300

    task11

---
+++ Task11/Program.cs
@@ -0,0 +1,11 @@
+﻿void number(int N)
+{
+int B=N%10;
+int C=N%100/10;
+if(B>C) {Console.Write(B);}
+if(C>B) {Console.Write(C);}
+if(C==B){Console.Write(C);}
+}
+int N=new Random().Next(10,99);
+Console.WriteLine(N);
+number(N);
