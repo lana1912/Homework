f0d1d49e860e0bf8deb5cff15b1129d6971c45a0
unknown <kovgankosvetlana.com>
2021-11-17 16:16:06 +0300

    task13

---
+++ Task13/Program.cs
@@ -0,0 +1,13 @@
+﻿//13.ВЫЯСНИТЬ, КРАТНО ЛИ ЧИСЛО ЗАДАННОМУ, ЕСЛИ НЕТ, ВВЫВЕСТИ ОСТАТОК.
+
+void number (int A, int B)
+{
+int C=0;
+if(B%A==0){Console.WriteLine("Число В кратно числу А");}
+else{C=B%A; Console.WriteLine($"Остаток от деления {C}");} 
+}
+Console.WriteLine("Введите число A : ");
+int A=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B : ");
+int B=Convert.ToInt32(Console.ReadLine());
+number(A, B);
