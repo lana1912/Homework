b3e71b4f4ffc325c79b2fb0fb13b1022f373a5d1
unknown <kovgankosvetlana.com>
2021-11-17 13:10:11 +0300

    task02

---
+++ Task02/Program.cs
@@ -0,0 +1,15 @@
+﻿ //02.ДАНЫ ДВА ЧИСЛА, ПОКАЗАТЬ НАИБОЛЬШЕЕ И НАИМЕНЬШЕЕ ЧИСЛО.
+
+void Method (int a, int b)
+{
+    int max=0;
+    int min=0;
+  if(a>b) {max=a;min=b;}
+  else{max=b; min=a;}
+  Console.WriteLine($"Максимальное число {max}, минимальное число {min}");
+}
+Console.WriteLine("Введите число а : ");
+ int a=Convert.ToInt32(Console.ReadLine());
+ Console.WriteLine("Введите число b : ");
+ int b=Convert.ToInt32(Console.ReadLine());
+Method(a,b);
