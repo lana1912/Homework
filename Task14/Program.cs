a0d08ba7152e4c50afb9232673e25c8e316516ff
unknown <kovgankosvetlana.com>
2021-11-17 16:22:48 +0300

    task14

---
+++ Task14/Program.cs
@@ -0,0 +1,11 @@
+﻿//14.НАЙТИ ТРЕТЬЮ ЦИФРУ ЧИСЛА ИЛИ СООБЩИТЬ ,ЧТО ЕЕ НЕТ.
+void number(int A)
+{
+int C=A%10;
+if(A/100>1) {Console.WriteLine(C);}
+else {Console.WriteLine("Третьей цифры нет");}
+}
+Console.WriteLine("Введите число ");
+int A=Convert.ToInt32(Console.ReadLine());
+number(A);
+
