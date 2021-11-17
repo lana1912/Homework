c7999e246b6b0265181946e9097f2b7d003de10b
unknown <kovgankosvetlana.com>
2021-11-17 16:05:48 +0300

    task12

---
+++ Task12/Program.cs
@@ -0,0 +1,10 @@
+﻿// Удалить вторую цифру трёхзначного числа
+
+void number(int A)
+{
+    int number2 = A/100*10+A%10;
+    Console.WriteLine(number2);
+}
+int A=new Random().Next(100,999);
+Console.WriteLine(A);
+number(A);
