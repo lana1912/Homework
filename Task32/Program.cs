2135b2a8f1174f63aceabab90e93889c9fe4853b
unknown <kovgankosvetlana.com>
2021-11-18 15:10:50 +0300

    task32

---
+++ Task32/Program.cs
@@ -0,0 +1,11 @@
+﻿//32.СОЗДАТЬ МАССИВ ИЗ 8 ЭЛЕМЕНТОВ, ЗАПОЛНЕННЫХ 0 И 1. ВЫВЕСТИ НА ЭКРАН.
+void number (int [] A)
+{
+  for(int i=0;i<A.Length;i++)
+  {   
+      A [i]=new Random().Next(2);
+      Console.Write($" { A [ i ] }");
+  }
+}
+int [] A= new int [8];
+number(A);
