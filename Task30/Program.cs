045fb97c1c23ef5de49e803295f23e2ffa846ede
unknown <kovgankosvetlana.com>
2021-11-18 14:04:02 +0300

    task30

---
+++ Task30/Program.cs
@@ -0,0 +1,17 @@
+﻿//30.ПОКАЗАТЬ КУБЫ ЧИСЕЛБ ЗАКАНЧИВАЮЩИХСЯ НА ЧЕТНУЮ ЦИФРУ.
+int number(int N)
+{
+    int i=1;
+    for ( i=1;i<=N;i++)
+    {
+        if (i%2==0)
+        {
+            Console.WriteLine($"Куб числа {i} = {i}*{i}*{i}");
+        }
+    
+    }    
+    return i;  
+}
+Console.WriteLine("Введите число : ");
+int N=Convert.ToInt32(Console.ReadLine());
+number (N);
