f65cac5e5b9f4ab61334d6d52efdf4226ada792c
unknown <kovgankosvetlana.com>
2021-11-18 16:38:01 +0300

    task34

---
+++ Task34/Program.cs
@@ -0,0 +1,17 @@
+﻿//34. Написать программу замену элементов массива на противоположные.
+void number (int [] N)
+{
+   for(int i=0;i<N.Length;i++)
+   {
+       N[i]=new Random().Next(-10,10);
+       Console.Write($"{N[i]} ");
+   }
+   Console.WriteLine();
+   for(int i=0;i<N.Length;i++)
+   {   
+    N[i]=N[i]*-1;
+    Console.Write($" {N[i]} ");
+   }
+}
+int [] N= new int [12];
+number(N);
