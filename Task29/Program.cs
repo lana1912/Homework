7939850dc888279241be7a6b81d9d921092d12eb
unknown <kovgankosvetlana.com>
2021-11-18 13:50:18 +0300

    task29

---
+++ Task29/Program.cs
@@ -0,0 +1,13 @@
+﻿//29.ПРОИЗВЕДЕНИЕ ЧИСЕЛ ОТ 1 ДО N
+int number (int N)
+{
+    int i=1;
+    int v=1;
+    for( i=1;i<=N;i++)
+    {v=v*i;}
+    Console.WriteLine(v);
+    return i;
+} 
+Console.WriteLine("Введите число : ");
+int N=Convert.ToInt32(Console.ReadLine());
+number(N);
