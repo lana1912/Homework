acb85cf5f1cb8c158e0f15a80f8fd0bb7ce348af
unknown <kovgankosvetlana.com>
2021-11-18 14:56:37 +0300

    task31

---
+++ Task31/Program.cs
@@ -0,0 +1,12 @@
+﻿//31.СОЗДАТЬ МАССИВ ИЗ 8 ЭЛЕМЕНТОВ, ВЫВЕСТИ МАССИВ.
+void number (int [] C)
+{
+for(int i=0;i<C.Length;i++)
+{
+    C[i]=new Random().Next(50);
+    Console.Write($" {C [i] } ");
+}
+}
+int [] C = new int [8];
+number(C);
+
