34d238d0beb9aaba992545a28c2d24e911b9a0dc
unknown <kovgankosvetlana.com>
2021-11-19 18:25:19 +0300

    task35

--- Task35/Program.cs
+++ Task35/Program.cs
@@ -1,29 +1,29 @@
 ﻿//35. Определить, присутствует ли в заданном массиве, некоторое число.
-void number(int [] N, int A)
+void number(int [] N)
 {
+    Console.WriteLine("Рандомный массив : ");
     for(int i=0;i<N.Length;i++)
     {
         N[i]=new Random().Next(0,15);
         Console.Write($" {N[i]} ");
     }
     Console.WriteLine();
-   for(int i=0;i<N.Length;i++)
-   {
-       if(N[i]==A)
-       {   
-           Console.WriteLine($"Число {A} есть в массиве");
-    
-       }
-       else{A=-1;}
-       
-   }
-   if(A==-1)
-       {
-           Console.WriteLine("Числа в массиве нет");
-       }
+  
 
 }
-int [] N=new int [10];
+int [] N=new int [12];
 Console.WriteLine("Введите число  А : ");
 int A=Convert.ToInt32(Console.ReadLine());
-number (N, A);
+number (N);
+
+string Find(int[] N)
+{
+     string result = "Числа нет ";
+    for (int i = 0; i <N.Length; i++)
+    {
+         if (N[i] == A) result = "Число есть";
+    }
+    return result;
+}
+Console.WriteLine(Find (N));
+
