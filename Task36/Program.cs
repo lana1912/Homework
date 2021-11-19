6ad03d6d0ecbd1ae0ccd19eb95cae33c0d4620a6
unknown <kovgankosvetlana.com>
2021-11-19 18:46:09 +0300

    task36

---
+++ Task36/Program.cs
@@ -0,0 +1,28 @@
+﻿//36. Задать массив, заполнить случайными положительными трёхзначными числами. Показать количество нечетных\четных чисел
+void number(int [] N)
+{
+    for(int i=0;i<N.Length;i++)
+    {
+        N[i]=new Random().Next(100,1000);
+        Console.Write($" {N[i]} ");
+    }
+Console.WriteLine();
+    int A=0;
+    int B=0;
+    for(int i=0;i<N.Length;i++)
+    {
+        if(N[i]%2==0)
+        {
+            A=A+1;
+        }
+        else
+        {
+            B=B+1;
+        }
+    }
+    Console.Write($"Четных чисел {A}");
+    Console.WriteLine();
+    Console.Write($"Нечетных чисел {B}");
+}
+int [] N=new int [15];
+number(N);
