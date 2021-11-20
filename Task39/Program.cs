712e381f79117dabdd33e471b38798c2eafb4df7
unknown <kovgankosvetlana.com>
2021-11-20 17:15:33 +0300

    task39

---
+++ Task39/Program.cs
@@ -0,0 +1,27 @@
+﻿void Array (int [] a)
+{
+    for(int i=0;i<a.Length;i++)
+    {
+        a[i]=new Random().Next(1,10);
+        Console.Write($" {a[i]} ");
+
+    }
+}
+Console.WriteLine("Одномерный массив");
+int [] a=new int [7];
+Array(a);
+Console.WriteLine();
+
+void Product(int [] a) 
+{ 
+    int j = a.Length-1; 
+    int count = a.Length/2; 
+    for(int i =0;  i <count; i++) 
+    { 
+         
+            Console.WriteLine($"{a[i]} * {a[j]} = {a[i] * a[j]}"); 
+            j--;            
+         
+    } 
+} 
+Product(a);
