08603331095b3c8c71b934b0d79b3c6534ac7e31
unknown <kovgankosvetlana.com>
2021-11-20 18:03:59 +0300

    task40

---
+++ Task40/Program.cs
@@ -0,0 +1,22 @@
+﻿//40. В Указанном массиве вещественных чисел найдите разницу между максимальным и минимальным элементом
+void Array (double [] a)
+{
+    for(int i=0;i<a.Length;i++)
+    {
+        a[i]=new Random().Next(1,10);
+    Console.Write($"{a[i]} ");
+    }
+}
+
+double Diff(double [] a)
+{
+    double m=a.Max<double>();
+    double n=a.Min<double>();
+    double diffr=m-n;
+    return diffr;
+}
+double [] a= new double [10];
+Array(a);
+Console.WriteLine();
+Console.WriteLine("Разница между максимальным и минимальным элементом");
+Console.WriteLine(Diff(a));
