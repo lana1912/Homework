2ef8ed8fd08b68b051478a94e08ad520ddab516b
unknown <kovgankosvetlana.com>
2021-11-20 20:03:05 +0300

    task43

---
+++ Task43/Program.cs
@@ -0,0 +1,7 @@
+﻿//43.Написать программу преобразования десятичного числа в двоичное.
+Console.WriteLine("Введите число"); 
+ 
+int number = Convert.ToInt32(Console.ReadLine()); 
+ 
+string s= Convert.ToString(number, 2); 
+Console.WriteLine("В двоичной системе счисления это: "+s);
