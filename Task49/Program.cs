32ffb8f39ac81af4150a1ababba7e0cd12c5bfc3
unknown <kovgankosvetlana.com>
2021-11-30 13:54:38 +0300

    task49

--- Task49/Program.cs
+++ Task49/Program.cs
@@ -3,16 +3,16 @@
 
 void FillArray(double [,] matr)
 {
-    for(double m=0; m<matr.GetLength(0); m++ )
+    for(int m=0; m<matr.GetLength(0); m++ )
     {
-        for(double n=0; n<matr.GetLength(1); n++)
+        for(int n=0; n<matr.GetLength(1); n++)
         {
-            matr[m,n]=new Random().NextDouble(1,30);
-        Console.Write($"{matr[m,n]}");
+            matr[m,n]=new Random().NextDouble();
+        Console.Write($"{matr[m,n]} ");
 
         }
         Console.WriteLine();
     }
-}
-double [,] matrix= new double [3,4];
+ }
+ double [,] matrix= new double [3,4];
 FillArray (matrix);
