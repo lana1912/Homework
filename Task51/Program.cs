373e44c276b479e7a0bbae9ca19a2d8752881aa8
unknown <kovgankosvetlana.com>
2021-11-30 14:43:44 +0300

    task51

---
+++ Task51/Program.cs
@@ -0,0 +1,18 @@
+﻿//51.Задать двумерный массив следующим правилом: Aₘₙ = m+n
+
+void Matrix(int[,] a)
+{
+    for (int i = 0; i < a.GetLength(0); i++)
+    {
+        for (int j = 0; j < a.GetLength(1); j++)
+        {
+            a[i, j] = i + j;
+            Console.Write($"{a[i, j]} ");
+
+        }
+        Console.WriteLine();
+    }
+}
+int[,] a = new int[4, 5];
+Matrix(a);
+
