4646a08be61019d9016d3d8d3e42a58220796600
unknown <kovgankosvetlana.com>
2021-11-30 14:18:13 +0300

    task50

---
+++ Task50/Program.cs
@@ -0,0 +1,37 @@
+﻿//50.В двумерном массиве заменить четные элементы на противоположные.
+
+void  Matrix(int [,] a )
+{
+    for(int i=0;i<a.GetLength(0); i++)
+    {
+        for(int j=0;j<a.GetLength(1); j++)
+        {
+            a[i,j]= new Random().Next(-5,20);
+            Console.Write($"{a[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void Opposite(int [,] a)
+{  
+    int m=-1;
+    for(int i=0;i<a.GetLength(0);i++)
+    {
+        for(int j=0;j<a.GetLength(1); j++)
+        {
+            if(a[i,j]%2==0)
+            {
+                a[i,j]=a[i,j]*m;
+
+            }
+            Console.Write($"{a[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int [,] a= new int [4,5];
+Matrix(a);
+Console.WriteLine();
+Opposite(a);
