5dc40ff0bce48d8504ce66ac297599d96edba09c
unknown <kovgankosvetlana.com>
2021-12-01 19:37:33 +0300

    Task54

---
+++ Task54/Program.cs
@@ -0,0 +1,37 @@
+﻿//54.В матрице чисел найти сумму элементов главной диагонали
+
+void Matrix(int [,] a)
+{
+    for(int i=0; i<a.GetLength(0); i++)
+    {
+        for(int j=0; j<a.GetLength(1); j++)
+        {
+            a[i,j]=new Random().Next(1,10);
+            Console.Write($"{a[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int sum=0;
+
+int Diagonal (int [,] a)
+{
+    for(int i=0; i<a.GetLength(0); i++)
+    {
+        for(int j=0; j<a.GetLength(1); j++)
+        {
+          if(i==j)
+          {
+              sum=sum+a[i,j];
+          } 
+
+        }
+    }
+   return sum;
+}
+
+int [,] a= new int [4,4];
+Matrix(a);
+Console.WriteLine("Сумма главной диагонали");
+Console.WriteLine(Diagonal(a));
