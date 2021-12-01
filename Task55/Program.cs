ff461f78b1dff288edb21182a3b1e56282b757c0
unknown <kovgankosvetlana.com>
2021-12-01 20:23:32 +0300

    Task55

---
+++ Task55/Program.cs
@@ -0,0 +1,30 @@
+﻿//Дан целочисленный массив. Найти среднее арифметическое каждого из столбцов.
+
+void Matrix(int [,] a)
+{
+    for(int i=0; i<a.GetLength(0); i++)
+    {
+        for(int j=0; j<a.GetLength(1); j++)
+        {
+            a[i,j]=new Random().Next(1,10);
+            Console.Write($"{a[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void Number(int [,] a)
+{   
+    int sum=0;
+    int j=0;
+    for (int i=0; i<a.GetLength(1); i++)
+    {
+        sum=sum+a[i,j];
+    }
+    
+}
+
+int [,] a= new int[4,4];
+Matrix(a);
+Number(a);
+
